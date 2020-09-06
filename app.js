907fe10b2e7bab098c33d6462699a697666607e0
Yossi Ben Zaken <yossibz@gmail.com>
2020-09-06 12:16:42 +0300

    Chat v1

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const appRoutes = require('./routes/app');
+
+const app = express();
+
+app.set('view', path.join(__dirname,'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/',appRoutes);
+
+app.use((req,res,next) => res.render('index'));
+
+module.exports = app;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChatRealTime';
+  user:String;
+  room:String;
+  messageText:String;
+  messageArray:Array<{user:String,message:String}> = [];
+  constructor(private _chatService:ChatService){
+      this._chatService.newUserJoined()
+      .subscribe(data=> this.messageArray.push(data));
+
+
+      this._chatService.userLeftRoom()
+      .subscribe(data=>this.messageArray.push(data));
+
+      this._chatService.newMessageReceived()
+      .subscribe(data=>this.messageArray.push(data));
+  }
+
+  join(){
+      this._chatService.joinRoom({user:this.user, room:this.room});
+  }
+
+  leave(){
+      this._chatService.leaveRoom({user:this.user, room:this.room});
+  }
+
+  sendMessage()
+  {
+      this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
+  }
 }
